51ff0ccdf8c682b648b6b9dd7ac3bf98ee348f58
Graham Schmidt <grahamschmidt@Grahams-MacBook-Pro.local>
2021-08-07 13:45:59 -0500

    added full message generating function, random choice function

--- messageGen.js
+++ messageGen.js
@@ -1,9 +1,11 @@
 // Program to randomly generate messages
 
-// Static lists of options for creating messages
+// Static arrays of options for creating messages
 let mealTimes = ['Breakfast', 'Lunch', 'Dinner'];
 let cuisines = ['Italian', 'American', 'Japanese'];
-let adjs = ['kick', 'sauce', 'surprise', 'je ne sais pas', 'something something'];
+let adjs = ['kick', 'extra \"sauce\"', 'surprise', 'je ne sais pas', 'something something'];
+let celebs = ['Vin Diesel', 'Cher', 'deadmau5', 'Dwayne \"The Rock\" Johnson'];
+let quotes = ['That\'s way too spicy for me', 'I love it! It\'s so healthy!', 'You could not pay me to eat that agian'];
 
 // Object for meal types
 let meals = {
@@ -13,3 +15,17 @@ let meals = {
         Japanese: ['Sushi', 'Ramen', 'Bento']
     }
 }
+
+// Dynamic function for picking random item from array no matter array length
+let randomChoice = array => {
+    let index = Math.floor(Math.random()*array.length);
+    return array[index];
+}
+
+// Function that generates and returns complete message
+let generateMessage = () => {
+    let cuisine = randomChoice(cuisines);
+    console.log(`Not sure what to have for ${randomChoice(mealTimes)} today? If you like ${cuisine} with a little ${randomChoice(adjs)}, may I suggest ${randomChoice(meals.cuisine[cuisine])}. We've heard ${randomChoice(celebs)} likes making this at home, and they even said \"${randomChoice(quotes)}\".`);
+}
+
+generateMessage();
