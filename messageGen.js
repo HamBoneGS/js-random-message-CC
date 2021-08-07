d1c54732fce93f328414db09801f7ca8bba18f34
Graham Schmidt <grahamschmidt@Grahams-MacBook-Pro.local>
2021-08-07 13:29:10 -0500

    Inital commit: start of message modules

---
+++ messageGen.js
@@ -0,0 +1,15 @@
+// Program to randomly generate messages
+
+// Static lists of options for creating messages
+let mealTimes = ['Breakfast', 'Lunch', 'Dinner'];
+let cuisines = ['Italian', 'American', 'Japanese'];
+let adjs = ['kick', 'sauce', 'surprise', 'je ne sais pas', 'something something'];
+
+// Object for meal types
+let meals = {
+    cuisine: {
+        Italian: ['Spaghetti', 'Pizza', 'Baked Ziti'],
+        American: ['Burger', 'Hot Dog', 'Chili'],
+        Japanese: ['Sushi', 'Ramen', 'Bento']
+    }
+}
